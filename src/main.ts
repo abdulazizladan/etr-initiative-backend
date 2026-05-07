dc41106e7b6a4de751cc9486ad93e855946f62ec
abdulazizladan <abdulazizladan@gmail.com>
2026-05-07 08:06:46 +0100

    task: Updates blogs endpoints

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
   console.log(`🚀 Server running on http://localhost:${process.env.PORT ?? 3000}`);
